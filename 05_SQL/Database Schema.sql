69829d2dcedfa3e0bb5e6a6e203d5d95cd1fc2fe
Jayasakthi <146506023+Jayasakthisiva@users.noreply.github.com>
2026-07-19 16:02:55 +0530

    Update Database Schema.sql

--- 05_SQL/Database Schema.sql
+++ 05_SQL/Database Schema.sql
@@ -17,34 +17,3 @@ DESCRIBE customers;
 DESCRIBE orders;
 
 DESCRIBE products;
-
-ALTER TABLE customers 
-MODIFY customer_id VARCHAR(10) NOT NULL;
-
-ALTER TABLE orders
-ADD CONSTRAINT fk_orders_customers
-FOREIGN KEY (customer_id)
-REFERENCES customers(customer_id);
-
-ALTER TABLE orders
-ADD CONSTRAINT fk_orders_products
-FOREIGN KEY (product_id)
-REFERENCES products(product_id);
-
-ALTER TABLE customers
-MODIFY registration_date date;
-
-ALTER TABLE orders
-MODIFY order_id VARCHAR (10) NOT NULL ;
-
-ALTER TABLE orders
-ADD PRIMARY KEY (order_id);
-
-ALTER TABLE orders 
-MODIFY order_date date; 
-
-ALTER TABLE orders 
-MODIFY COLUMN delivery_date date; 
-
-ALTER TABLE orders
-MODIFY customer_rating DECIMAL(3,1);
